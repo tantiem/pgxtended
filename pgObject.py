d74b7dd2f6c1fe4ba27c7b3412cbcca35f9aeb42
Levi <thesealeviathan@gmail.com>
2021-10-05 16:55:43 -0600

    Added base project files

---
+++ pgObject.py
@@ -0,0 +1,8 @@
+import pygame
+
+class pgObject:
+	def __init__(self, pos_vector):
+		self.position = pygame.math.Vector2(pos_vector)
+
+	def inRange(self, currentCamera):
+		#return if this object is in view of the current camera
