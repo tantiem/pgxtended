960f7ff2df8435a76dd80949fcf53c1681239982
Levi <thesealeviathan@gmail.com>
2021-10-07 22:58:31 -0600

    remove vestiges

--- pgObject.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class pgObject:
-	def __init__(self, pos_vector):
-		self.position = pygame.math.Vector2(pos_vector)
-
-	def inRange(self, currentCamera):
-		#return if this object is in view of the current camera
