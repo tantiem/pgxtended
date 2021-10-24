65fe621c53eaecb64be533729bde3c4b779cc2fe
Levi <thesealeviathan@gmail.com>
2021-10-24 11:07:26 -0600

    Made major changes to Camera. Can zoom, scroll. Support for multiple camera views

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ FPSclock = pygame.time.Clock()
 scrX = 1080
 scrY = 720
 
-screen = pygame.display.set_mode((scrX, scrY))
+P_DISPLAY = pygame.display.set_mode((scrX, scrY))
+screen = pygame.Surface((scrX,scrY))
 
 G_SPR_MAP = pygame.sprite.Group()
 G_SPR_UI = pygame.sprite.Group()
@@ -24,13 +25,24 @@ img1 = pygame.image.load("giornoemoji.png").convert_alpha()
 
 obj1 = Dynamic.Dynamic(pygame.math.Vector2(0,0),img1,G_SPR_MAP)
 
-main_cam = Camera.Camera(pygame.math.Vector2(0,0))
-camSensitivity = 1.0
-buttons = []
+class Button(UI.UI):
+    def __init__(self,pos,img,group=None,layer=0):
+        super().__init__(pos,img,group,layer)
 
+    def OnClick(self):
+        print("clicked")
 
+newButton = Button(pygame.math.Vector2(0,0),img1,G_SPR_UI)
+
+main_cam = Camera.Camera(pygame.math.Vector2(0,0),(scrX,scrY),(0,0),screen)
+camSensitivity = 2
+
+buttons = [newButton]
+
+zoom = 1
 running = True
 while running:
+    
     '''Held down key input
     ALSO we are doing some neat camera movement right here specifically, acceleration and stuff so far.'''
     held_keys = pygame.key.get_pressed()
@@ -50,6 +62,7 @@ while running:
         if held_keys[pygame.K_RIGHT]:
             main_cam.MoveCam(pygame.math.Vector2(1,0) * camSensitivity)
     
+
     '''End Camera Movement'''
     '''Start input'''
     for event in pygame.event.get():
@@ -57,22 +70,29 @@ while running:
             running = False
         if event.type == locs.KEYDOWN:
             '''Key Presses'''
+            if event.key == locs.K_w:
+                zoom += 1
+            if event.key == locs.K_s:
+                zoom -= 1
         if event.type == locs.MOUSEBUTTONDOWN:
             '''Mouse Cliques'''
             for button in buttons:
                 if button.rect.collidepoint(mouse_pos):
-                    pass
+                    button.OnClick()
         if event.type == locs.MOUSEMOTION:
             pass
             '''Mouse Movement'''
     '''While loop updates'''
-    screen.fill((255, 255, 255))
+    main_cam.SetCamZoom(zoom,P_DISPLAY)
+    main_cam.Clear()
     '''DrawStuff'''
+    pygame.draw.rect(main_cam.camSurface, (100,100,100), newButton.rect)
     G_SPR_MAP.update(main_cam)
     G_SPR_UI.update(main_cam)
     '''Updating done'''
-    G_SPR_MAP.draw(screen)
-    G_SPR_UI.draw(screen)
+    G_SPR_MAP.draw(main_cam.camSurface)
+    G_SPR_UI.draw(main_cam.camSurface)
+    
     pygame.display.flip()
     FPSclock.tick(FPS)
 else:
--- pgx/Camera.py
+++ pgx/Camera.py
@@ -1,18 +1,47 @@
 from . import Undrawable
 from pygame import math
+from pygame import transform
+from pygame import surface
 
 #local pos is always 0
 
 class Camera(Undrawable.Undrawable):
-    def __init__(self, pos):
+    def __init__(self, pos, viewport, origin, camSurface):
         super().__init__(pos)
+        #viewport: (private) tuple: Describes the height and width of the viewport 
+        #origin: (private) tuple: Describes the origin of this camera
+        #camSurface: (public) Surface: The surface that acts as this cameras view frustrum onto
+        self._viewport = viewport
+        self._origin = origin
+        self.camSurface = camSurface
 
     def SetCamPos(self, absolutePos):
-        pass
+        #Inherited: self.gPosition (Vector2) - Global position
+        self.gPosition = absolutePos
 
     def MoveCam(self, relativePos):
         #Inherited: self.gPosition (Vector2) - Global position
         self.gPosition += relativePos
 
-    def SetCamZoom(self, absoluteMultiplier):
+    def SetCamZoom(self, absoluteMultiplier, pygameDisplaySurface):
+        if(absoluteMultiplier < 0):
+            print(f"Attempted to set negative zoom on {self}")
+            return
+        #Transform scale the screen.
+        #Used to set the scale factor of the camera. Will zoom in and out based on topleft.
+        #If you want to zoom in on, say the center, simply move the camera at the same time as calling this;
+        #i.e; zoom to 2x, move the camera half viewport length left and up.
+
+        #Set the new viewport size
+        newViewportTuple = (int(self._viewport[0] * absoluteMultiplier),int(self._viewport[1] * absoluteMultiplier))
+        #Create the new viewport surface
+        newSurface = surface.Surface(newViewportTuple)
+        newSurface.blit(self.camSurface, (0,0))
+        
+        pygameDisplaySurface.blit(transform.scale(self.camSurface,self._viewport),self._origin)
+        self.camSurface = newSurface
+
         pass
+
+    def Clear(self):
+        self.camSurface.fill((255,255,255))
